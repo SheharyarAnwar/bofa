37e73702c04dfb7ba6b30fda91d7a8a2865b5fd9
Sheharyar Anwar <sheharyar28@hotmail.com>
2024-10-13 13:42:49 +0500

    wong lp

--- apps/web/app/components/Skills/Skills.tsx
+++ apps/web/app/components/Skills/Skills.tsx
@@ -1,12 +1,10 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useLayoutEffect } from 'react';
 import axios from 'axios';
 import { ForceGraph2D } from 'react-force-graph';
 import { Skill } from '../../types/skill';
-import { Select, Button } from 'antd';
-
-const { Option } = Select;
+import { Select, Button, Form, Col } from 'antd';
 
 const API_URL = 'http://localhost:3000/skills';
 
@@ -16,7 +14,15 @@ interface GraphData {
 }
 
 const Skills: React.FC = () => {
-  const [graphData, setGraphData] = useState<GraphData>({ nodes: [], links: [] });
+  const [graphData, setGraphData] = useState<GraphData>({
+    nodes: [],
+    links: [],
+  });
+  const [allSkills, setAllSkills] = useState<Skill[]>([]);
+  const [form] = Form.useForm();
+  const graphContainerRef = useRef<HTMLDivElement>(null);
+  const [graphWidth, setGraphWidth] = useState(0);
+  const [graphHeight, setGraphHeight] = useState(0);
   const [currentSkills, setCurrentSkills] = useState<string[]>([]);
   const [desiredSkills, setDesiredSkills] = useState<string[]>([]);
   const [learningPath, setLearningPath] = useState<string[]>([]);
@@ -25,155 +31,203 @@ const Skills: React.FC = () => {
     fetchSkills();
   }, []);
 
+  const findOptimalLearningPath = (graphData: GraphData, currentSkills: string[], desiredSkills: string[]): string[] => {
+    const queue: { skill: string; path: string[] }[] = [];
+    const visited = new Set<string>();
+  
+    // Initialize the queue with current skills
+    currentSkills.forEach(skill => {
+      queue.push({ skill, path: [skill] });
+      visited.add(skill);
+    });
+  
+    while (queue.length > 0) {
+      const { skill, path } = queue.shift()!;
+  
+      // Check if the current skill is in the desired skills
+      if (desiredSkills.includes(skill)) {
+        return path; // Return the path if a desired skill is found
+      }
+  
+      // Find related skills
+      const relatedLinks = graphData.links.filter(link => link.source === skill);
+      relatedLinks.forEach(link => {
+        const nextSkill = link.target;
+        if (!visited.has(nextSkill)) {
+          visited.add(nextSkill);
+          queue.push({ skill: nextSkill, path: [...path, nextSkill] });
+        }
+      });
+    }
+  
+    return []; // Return an empty array if no path is found
+  };
+
   const fetchSkills = async () => {
     try {
       const response = await axios.get<Skill[]>(API_URL);
+      setAllSkills(response.data);
       const skills = response.data;
-      const nodes = skills.map(skill => ({ id: skill.id, name: skill.name }));
-      const links = skills.flatMap(skill => 
-        (skill.subSkills || []).map(subSkillId => ({ source: skill.id, target: subSkillId }))
-          .concat((skill.relatedSkills || []).map(relatedSkillId => ({ source: skill.id, target: relatedSkillId })))
+      const nodes = skills.map((skill) => ({ id: skill._id, name: skill.name }));
+  
+      const links = skills.flatMap((skill) =>
+        (skill.subSkills || []).map((subSkillId) => ({
+          source: skill._id,
+          target: subSkillId,
+          type: 'subSkill', // Indicate this is a subSkill relationship
+        }))
+        .concat(
+          (skill.relatedSkills || []).map((relatedSkillId) => ({
+            source: skill._id,
+            target: relatedSkillId,
+            type: 'relatedSkill', // Indicate this is a relatedSkill relationship
+          })),
+        ),
       );
+  
       setGraphData({ nodes, links });
     } catch (error) {
       console.error('Error fetching skills:', error);
     }
   };
 
-  const calculateLearningPath = () => {
-    const graph = buildGraph(graphData);
-    const paths = dijkstra(graph, currentSkills, desiredSkills);
-    setLearningPath(paths);
-  };
-
-  const buildGraph = (data: GraphData) => {
-    const graph: { [key: string]: { [key: string]: number } } = {};
-    data.links.forEach(link => {
-      if (!graph[link.source]) graph[link.source] = {};
-      //@ts-ignore
-      graph[link.source][link.target] = 1; // Assuming each edge has a weight of 1
+  const handleSubmit = () => {
+    form.validateFields().then((values) => {
+      const currentSkills = values.currentSkills; // Get current skills from form
+      const desiredSkills = values.desiredSkills; // Get desired skills from form
+  
+      // Calculate the optimal learning path
+      const learningPath = findOptimalLearningPath(graphData, currentSkills, desiredSkills);
+  
+      // Display the learning path
+      console.log('Optimal Learning Path:', learningPath);
+      setLearningPath(learningPath);
     });
-    return graph;
   };
 
-  const dijkstra = (graph: { [key: string]: { [key: string]: number } }, startSkills: string[], endSkills: string[]) => {
-    const distances: { [key: string]: number } = {};
-    const previous: { [key: string]: string | null } = {};
-    const queue: string[] = [];
-
-    // Initialize distances and queue
-    for (const node in graph) {
-      distances[node] = Infinity;
-      previous[node] = null;
-      queue.push(node);
+  useLayoutEffect(() => {
+    if (graphContainerRef.current) {
+      // get exact height and width of the container considering padding
+      setGraphWidth(graphContainerRef.current.clientWidth - 24);
+      setGraphHeight(graphContainerRef.current.clientHeight - 24);
     }
+  }, []);
 
-    // Set the distance for current skills to 0
-    startSkills.forEach(skill => {
-      distances[skill] = 0;
-    });
-
-    while (queue.length > 0) {
-      // Get the node with the smallest distance
-      const currentNode = queue.reduce((minNode, node) => 
-      //@ts-ignore
-
-        distances[node] < distances[minNode] ? node : minNode
-      );
-
-      // Remove the current node from the queue
-      queue.splice(queue.indexOf(currentNode), 1);
-
-      // If the current node is one of the desired skills, reconstruct the path
-      if (endSkills.includes(currentNode)) {
-        return reconstructPath(previous, currentNode);
-      }
-
-      // Explore neighbors
-      for (const neighbor in graph[currentNode]) {
-      //@ts-ignore
-
-        const alt = distances[currentNode] + graph[currentNode][neighbor];
-      //@ts-ignore
-
-        if (alt < distances[neighbor]) {
-          distances[neighbor] = alt;
-          previous[neighbor] = currentNode;
-        }
-      }
-    }
-
-    return []; // Return an empty path if no path is found
-  };
-
-  const reconstructPath = (previous: { [key: string]: string | null }, endNode: string) => {
-    const path: string[] = [];
-    let currentNode: string | null = endNode;
-
-    while (currentNode) {
-      path.unshift(currentNode);
-      //@ts-ignore
-
-      currentNode = previous[currentNode];
-    }
-
-    return path;
-  };
 
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <h1>Skills Graph</h1>
-      <ForceGraph2D
-        graphData={graphData}
-        nodeLabel={(node) => node.name} // Change to display node name as text
-        nodeAutoColorBy="id"
-        linkDirectionalParticles={2}
-        linkDirectionalParticleSpeed={0.001}
-      />
-      <div>
-        <h2>Select Current Skills</h2>
-        <Select
-          mode="multiple"
-          style={{ width: '100%' }}
-          placeholder="Select your current skills"
-          onChange={setCurrentSkills}
-        >
-          {graphData.nodes.map(skill => (
-            <Option key={skill.id} value={skill.id}>
-              {skill.name}
-            </Option>
-          ))}
-        </Select>
+    <>
+      <Form form={form} className="flex flex-col gap-4 mt-4 ">
+        <Col span={12}>
+          <Form.Item
+            label={<p >Current Skills</p>}
+            name="currentSkills"
+          >
+            <Select
+              mode="multiple"
+              filterOption={(input, option) =>
+                (option?.label ?? '')
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              placeholder="Select Current Skills"
+              options={allSkills.map((skill) => ({
+                label: skill.name,
+                value: skill._id,
+              }))}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item
+            label={<p >Select Desired Skills</p>}
+            name="desiredSkills"
+          >
+            <Select
+              mode="multiple"
+              filterOption={(input, option) =>
+                (option?.label ?? '')
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              placeholder="Select Desired Skills"
+              options={allSkills.map((skill) => ({
+                label: skill.name,
+                value: skill._id,
+              }))}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Button type="primary" onClick={handleSubmit}>
+            Generate Learning Path
+          </Button>
+        </Col>
+      </Form>
+      <div ref={graphContainerRef} className='p-6 flex-1'>
+        <ForceGraph2D
+          graphData={graphData}
+          width={graphWidth}
+          height={graphHeight}
+          nodeAutoColorBy="group"
+          linkCanvasObject={(link, ctx) => {
+            ctx.beginPath();
+            //@ts-ignore
+            ctx.moveTo(link.source.x, link.source.y);
+            //@ts-ignore
+            ctx.lineTo(link.target.x, link.target.y);
+        
+            // Set the stroke style based on the type of link
+            if (link.type === 'subSkill') {
+              ctx.strokeStyle = 'grey'; // Solid line for subSkills
+              ctx.lineWidth = 1; // Solid line width
+              ctx.stroke();
+            } else if (link.type === 'relatedSkill') {
+              ctx.strokeStyle = 'grey'; // Dashed line for relatedSkills
+              ctx.lineWidth = 1; // Dashed line width
+              ctx.setLineDash([5, 5]); // Set dash pattern
+              ctx.stroke();
+              ctx.setLineDash([]); // Reset dash pattern for future lines
+            }
+          }}
+          nodeCanvasObject={(node, ctx, globalScale) => {
+            const label = node.name;
+            const fontSize = 12/globalScale;
+            ctx.font = `${fontSize}px Sans-Serif`;
+            const textWidth = ctx.measureText(label).width;
+            const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            //@ts-ignore
+            ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+            
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = node.color;
+            //@ts-ignore
+            ctx.fillText(label, node.x, node.y);
+            
+            node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+          }}
+          nodePointerAreaPaint={(node, color, ctx) => {
+            ctx.fillStyle = color;
+            const bckgDimensions = node.__bckgDimensions;
+            //@ts-ignore
+            bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+          }}
+        />
+      
+      
       </div>
       <div>
-        <h2>Select Desired Skills</h2>
-        <Select
-          mode="multiple"
-          style={{ width: '100%' }}
-          placeholder="Select your desired skills"
-          onChange={setDesiredSkills}
-        >
-          {graphData.nodes.map(skill => (
-            <Option key={skill.id} value={skill.id}>
-              {skill.name}
-            </Option>
-          ))}
-        </Select>
-      </div>
-      <Button type="primary" onClick={calculateLearningPath} style={{ marginTop: '16px' }}>
-        Calculate Learning Path
-      </Button>
-      {learningPath.length > 0 && (
-        <div>
-          <h2>Learning Path</h2>
-          <ul>
-            {learningPath.map(skill => (
-              <li key={skill}>{skill}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <h3>Optimal Learning Path:</h3>
+      <ul>
+        {learningPath.map(skillId => {
+          const skill = allSkills.find(s => s._id === skillId);
+          return <li key={skillId}>{skill ? skill.name : skillId}</li>;
+        })}
+      </ul>
     </div>
+    </>
   );
 };
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import Skills from './components/Skills/Skills';
 import styles from './page.module.css';
+import './globals.css';
 
 
 const RootPage = ({ params }: { params: { forTest?: boolean } }) => {
   return (
     <main className={styles.main}>
+      <h2 className='mx-auto'>Skill Visualizer</h2>
       <Skills />
     </main>
   );
